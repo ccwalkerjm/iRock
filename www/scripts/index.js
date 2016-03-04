6cd1f1e7ba16917355f392e85b1ffdf6b536f15c
ccwalkerjm <ccwalkerjm@gmail.com>
2016-03-03 20:20:57 -0500

    fixed signature
    signature now working. Signature page must be first

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -2,19 +2,22 @@
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
+
 var sigCapture = null;
 
 $(document).ready(function (e) {
-
     sigCapture = new SignatureCapture("signature");
+
+    $(':mobile-pagecontainer').pagecontainer('change', '#main-page');
 
     $('#submit-btn').click(function () {
         var sig = sigCapture.toString();
         alert(sig);
 
-    });    
+    });
 
 });
+
 
 document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
@@ -22,12 +25,7 @@ function onDeviceReady() {
     // Handle the Cordova pause and resume events
     document.addEventListener('pause', onPause.bind(this), false);
     document.addEventListener('resume', onResume.bind(this), false);
-
-   
-    $(document).on("pagebeforeshow", "#page-signature", function () {
        
-    });
-    // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
 };
 
 function onPause() {
