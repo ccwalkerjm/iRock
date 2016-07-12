fbcbeadb5f7eb84e096f3f13dff6d098a24641a6
ccwalkerjm <ccwalkerjm@gmail.com>
2016-07-12 13:08:54 -0500

    switch to local storage

---
+++ www/scripts/initPage.js
@@ -0,0 +1,11 @@
+﻿$(document).on("mobileinit", function () {
+    $.mobile.autoInitializePage = false;
+});
+
+
+//$(document).ready(function (e) {
+//    window.location.hash = 'main-page';
+//    // #initialise jQM
+//    $.mobile.initializePage();
+//});
+
